506e19d11f78a1440d95ef959aaa85b09fddb065
maxprodigy <peterphilipjohnson@gmail.com>
2023-03-17 13:06:20 -0700

    Do Hard Things

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 when successful
+ */
+
+int main(void)
+
+{
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
+	putchar(d);
+	putchar('\n');
+	return (0);
+}
