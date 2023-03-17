4e93c510185317ed5cb8e2801a76a373dac2f05b
maxprodigy <peterphilipjohnson@gmail.com>
2023-03-17 14:03:25 -0700

    Do Hard Things

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -4,21 +4,20 @@
 
 /**
  * main - Entry point
- * Return - Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 
-
 int main(void)
-
 {
-	int d;
-	char low;
+       	int d;
+        char low;
 
-	for (d = '0'; d <= '9'; d++)
-	putchar(d);
+        for (d = '0'; d <= '9'; d++)
+        putchar(d);
 
-	for (low = 'a'; low <= 'f'; low++)
-	putchar(low);
-	putchar('\n');
-	return (0);
+        for (low = 'a'; low <= 'f'; low++)
+        putchar(low);
+        putchar('\n');
+       	return (0);
 }
+
