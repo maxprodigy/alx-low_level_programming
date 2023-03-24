9b8bb5d4a9ae21919a95bc3e0ed34720691a1069
maxprodigy <peterphilipjohnson@gmail.com>
2023-03-23 17:31:58 -0700

    kool aid

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isdigit -  check if a charater is a digit
+ * @x: The number to be checked
+ *Return: 1 for a character that will be a digit or 0 for anything else
+ /
+
+ 	int _isdigit(int x)
+
+	if (x >= 48 && 57)
+	{
+	return (1);
+	}
+	return (0);
