fec33a34f8dd6338e137611a35be2c935f9499a7
maxprodigy <peterphilipjohnson@gmail.com>
2023-03-29 16:46:13 -0700

    Boss

--- pointers/0-strcat.c
+++ pointers/0-strcat.c
@@ -1,19 +1,21 @@
 #include "main.h"
 
 /**
- * *_strcat - function commute srtings
- * @dest: param pointer to a char
- * @src: param pointer to a char
+ * *_strcat - commutes with the strings
+ * @dest: param pointer to char
+ * @src: param pointer to char
  * Return: return value of dest
  */
 
 char *_strcat(char *dest, char *src)
+
 {
 	int i;
 	int j;
 
 	i = 0;
 	j = 0;
+
 
 	while (dest[i] != '\0')
 	{
