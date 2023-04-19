21aa6720ee8531c7e621d7643f7bf0fa92ee9731
maxprodigy <peterphilipjohnson@gmail.com>
2023-04-19 12:46:22 -0700

    Killing It

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "3-calc.h"
 
 /**
@@ -10,32 +12,23 @@
 
 int main(int argc, char *argv[])
 {
-	int x, y;
-	int (*operation)(int, int);
+        int (*oprt)(int, int);
 
-	if (argc != 4)
-	{
-		printf("Error\n");
-		exit(98);
-	}
+        if (argc != 4)
+        {
+                printf("Error\n");
+                exit(98);
+        }
 
-	if (argv[2][1])
-	{
-		printf("Error\n");
-		exit(99);
-	}
+        oprt = get_op_func(argv[2]);
 
-	operation = get_op_func(argv[2]);
+        if (!oprt)
+        {
+                printf("Error\n");
+                exit (99);
 
-	if (operation == NULL)
-	{
-		printf("Error\n");
-		exit(99);
-	}
+        }
 
-	x = atoi(argv[1]);
-	y = atoi(argv[3]);
-
-	printf("%d\n", operation(x, y));
-	return (0);
+        printf("%d\n", oprt(atoi(argv[1]), atoi(argv[3])));
+        return (0);
 }
